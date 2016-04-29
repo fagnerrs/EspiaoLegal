7e5ff7871c0f9c4bb344c1859150452fea92f6d7
Fagner Oliveira <fagnerrs@gmail.com>
2016-04-29 17:14:24 -0300

    update base name

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ db.once('open', function () {
 });
 
 var server = http.createServer(function(req, res){
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+res.end('<html><body><h1>Hello World</h1></body></html>');
 });
 
 // start http server
